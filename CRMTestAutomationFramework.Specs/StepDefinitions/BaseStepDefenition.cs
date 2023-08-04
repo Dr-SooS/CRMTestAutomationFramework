67075203af2b6c45ddddb49c6115f723c47ffcd5
Vladislav Dobritski <915573@gmail.com>
2023-08-04 16:01:20 +0200

    Add Login via API

--- CRMTestAutomationFramework.Specs/StepDefinitions/BaseStepDefenition.cs
+++ CRMTestAutomationFramework.Specs/StepDefinitions/BaseStepDefenition.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using CRMTestAutomationFramework.Specs.Configuration;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
 
 namespace CRMTestAutomationFramework.Specs.StepDefinitions
 {
@@ -21,7 +23,7 @@ namespace CRMTestAutomationFramework.Specs.StepDefinitions
         public static HomeDashboardPage HomeDashboardPage;
 
         [BeforeTestRun]
-        public static void BeforeTest()
+        public static async Task BeforeTest()
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
@@ -29,12 +31,22 @@ namespace CRMTestAutomationFramework.Specs.StepDefinitions
             var user = config.GetSection("User").Get<User>();
             var siteSettings = config.GetSection("SiteSettings").Get<SiteSettings>();
 
+            var content = new StringContent($"{{\"username\": \"{user.Username}\", \"password\": \"{user.Password}\"}}", Encoding.UTF8, "application/json");
+            var responce = await new HttpClient()
+                .PostAsync($"{siteSettings.BaseUrl}/json.php?action=login", content);
+            var responceBody = JObject.Parse(await responce.Content.ReadAsStringAsync());
+
+            
+
+
             WebDriver = new ChromeDriver();
             WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
             WebDriver.Manage().Window.Maximize();
             WebDriver.Navigate().GoToUrl(siteSettings.BaseUrl);
-            LoginPage = new LoginPage(WebDriver);
-            HomeDashboardPage = LoginPage.Login(user.Username, user.Password);
+            var cookie = new Cookie("PHPSESSID", responceBody.Value<string>("json_session_id"));
+            WebDriver.Manage().Cookies.AddCookie(cookie);
+            WebDriver.Navigate().GoToUrl($"{siteSettings.BaseUrl}/index.php");
+            HomeDashboardPage = new HomeDashboardPage(WebDriver);
             HomeDashboardPage.Wait().Until(WebDriver => HomeDashboardPage.PageTitile.Displayed);
         }
 
